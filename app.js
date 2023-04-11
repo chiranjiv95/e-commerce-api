627c8fb74afa0008b726a659d448ba750170286c
chiranjiv95 <chiranjiv.mohanty10@gmail.com>
2023-04-11 05:18:18 -0700

    changes in docs

--- app.js
+++ app.js
@@ -31,11 +31,6 @@ app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.static('./public'));
 app.use(fileUpload());
 
-// temp testing route
-app.get('/api/v1', (req, res)=>{
-    console.log(req.signedCookies);
-    res.status(200).send(`E-Commerce API`);
-})
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
